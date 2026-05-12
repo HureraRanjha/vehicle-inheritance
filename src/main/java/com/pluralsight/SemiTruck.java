239c445a96213c719aba1661406eda529d61aa8d
HureraRanjha <hureraranjha9@gmail.com>
2026-05-12 10:48:20 -0400

    Done creating constructors for child classes

--- src/main/java/com/pluralsight/SemiTruck.java
+++ src/main/java/com/pluralsight/SemiTruck.java
@@ -2,4 +2,8 @@ package com.pluralsight;
 
 public class SemiTruck extends Vehicle
 {
+    public SemiTruck(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity)
+    {
+        super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
+    }
 }
